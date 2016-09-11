25e58e03c87ad85fc0bbe0cac0d8e6fd4875b92f
Lucas Andres Lizama Monje <lucaslizama3@hotmail.com>
2016-09-11 16:55:07 -0300

    Agregada clase de pruebas para Cotizador

---
+++ test/cotizador/CotizadorTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cotizador;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lucas
+ */
+public class CotizadorTest {
+    
+    public CotizadorTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
